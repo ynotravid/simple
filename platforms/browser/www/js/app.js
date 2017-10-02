6a7bd7a3094fa6dd752fe6dfa57895b43aed5bf0
ynotravid <ynotravid@gmail.com>
2017-10-01 20:18:11 -0700

    Setting up initial layout

---
+++ platforms/browser/www/js/app.js
@@ -0,0 +1,73 @@
+// Init App
+var myApp = new Framework7({
+    modalTitle: 'Framework7',
+    // Enable Material theme
+    material: true,
+});
+
+// Expose Internal DOM library
+var $$ = Dom7;
+
+// Add main view
+var mainView = myApp.addView('.view-main', {
+});
+mainView.router.loadPage('scorecard.html');
+// Add another view, which is in right panel
+// var rightView = myApp.addView('.view-right', {
+//     name: 'right'
+// });
+
+// Show/hide preloader for remote ajax loaded pages
+// Probably should be removed on a production/local app
+$$(document).on('ajaxStart', function (e) {
+    if (e.detail.xhr.requestUrl.indexOf('autocomplete-languages.json') >= 0) {
+        // Don't show preloader for autocomplete demo requests
+        return;
+    }
+    myApp.showIndicator();
+});
+$$(document).on('ajaxComplete', function (e) {
+    if (e.detail.xhr.requestUrl.indexOf('autocomplete-languages.json') >= 0) {
+        // Don't show preloader for autocomplete demo requests
+        return;
+    }
+    myApp.hideIndicator();
+});
+
+
+
+/* ===== Preloader Page events ===== */
+myApp.onPageInit('preloader', function (page) {
+    $$('.demo-indicator').on('click', function () {
+        myApp.showIndicator();
+        setTimeout(function () {
+            myApp.hideIndicator();
+        }, 2000);
+    });
+    $$('.demo-preloader').on('click', function () {
+        myApp.showPreloader();
+        setTimeout(function () {
+            myApp.hidePreloader();
+        }, 2000);
+    });
+    $$('.demo-preloader-custom').on('click', function () {
+        myApp.showPreloader('My text...');
+        setTimeout(function () {
+            myApp.hidePreloader();
+        }, 2000);
+    });
+});
+
+
+
+/* ===== Change statusbar bg when panel opened/closed ===== */
+$$('.panel-left').on('open', function () {
+    $$('.statusbar-overlay').addClass('with-panel-left');
+});
+
+$$('.panel-left').on('close', function () {
+    $$('.statusbar-overlay').removeClass('with-panel-left');
+});
+
+
+
